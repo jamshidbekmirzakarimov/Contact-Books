cc253ee15a5dc22fb9537a62b8519e2c56238974
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-02-08 21:02:37 +0500

    get element

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-let a = "12";
+//GET ELEMENT
+const elContactForm = document.querySelector(".js-contact-form");
+const elContactInputName = elContactForm.querySelector(".js-contact-name");
+const elContactInputRel = elContactForm.querySelector(".js-contact-relationship");
+const elContactInputNumber = elContactForm.querySelector(".js-contact-phone-number");
+const elContactList = document.querySelector(".js-contact-list");
