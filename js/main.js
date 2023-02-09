b705223561ba6edf064cea68f963de167d2fa011
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-02-09 20:02:19 +0500

    bitta arrayga push qilib oldik

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+const arrayContacts = [];
+
 //GET ELEMENT
 const elContactForm = document.querySelector(".js-contact-form");
 const elContactInputName = elContactForm.querySelector(".js-contact-name");
 const elContactInputRel = elContactForm.querySelector(".js-contact-relationship");
 const elContactInputNumber = elContactForm.querySelector(".js-contact-phone-number");
 const elContactList = document.querySelector(".js-contact-list");
+
+
+const arrayPush = (userName, relationship, phoneNumber) => {
+  arrayContacts.push({
+    name: userName,
+    rel: relationship,
+    phone: phoneNumber
+  });
+};
+
+
